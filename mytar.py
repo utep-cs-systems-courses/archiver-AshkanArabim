4e2f07a3674f459bb957c823c1d039a3aaac8794
AshkanArabim <ashkan.arabim@gmail.com>
2024-03-02 00:00:54 -0700

    rewrite interface to prep for inband framing

--- mytar.py
+++ mytar.py
@@ -1,13 +1,15 @@
 #! /usr/bin/env python3
-import sys
 import os
 import struct
-import re
+import argparse
+
+
+class InboundFramer:
+    pass
 
 
 class Framer:
-    # note: I'm using a header size of 64 bits to store file / filename size
-    def __init__(self, filename: str, archive_fd: int):
+    def __init__(self, framer_type: str, filename: str, archive_fd: int):
         # save archive fd
         self.archive_fd = archive_fd
 
@@ -16,37 +18,128 @@ class Framer:
 
         # save file name
         self.filename = filename
+        
+        # class bodies will differ based on the framer type
+        # note: for out-of-bound, I'm using a header size of 64 bits to store file / filename size
+        self.framer_type = framer_type
+        self.start_frame = self._define_start_frame()
+        self.write_frame = self._define_write_frame()
+        self.end_frame = self._define_end_frame()
+        self.close = self._define_close()
+        
+    def _define_start_frame(self):
+        if (self.framer_type == "out"):
+            def start_frame():
+                # get the length of filename, format in 64 bits
+                b = binary_format_64(len(self.filename))
 
-    def start_frame(self):
-        # get the length of filename, format in 64 bits
-        b = binary_format_64(len(self.filename))
+                # write the filename size and filename
+                os.write(self.archive_fd, b)
+                os.write(self.archive_fd, self.filename.encode())
 
-        # write the filename size and filename
-        os.write(self.archive_fd, b)
-        os.write(self.archive_fd, self.filename.encode())
+                # get the content size, format it
+                b = binary_format_64(os.fstat(self.file_fd).st_size)
 
-        # get the content size, format it
-        b = binary_format_64(os.fstat(self.file_fd).st_size)
+                # write file size
+                os.write(self.archive_fd, b)
+            return start_frame
+        else:
+            # TODO: out of bound frame writer
+            pass
 
-        # write file size
-        os.write(self.archive_fd, b)
+    def _define_write_frame(self):
+        if (self.framer_type == "out"):
+            def write_frame():
+                # write the whole file
+                while True:
+                    buffer = os.read(self.file_fd, 100)
+                    l = len(buffer)
+                    if l == 0:
+                        break
+                    os.write(self.archive_fd, buffer)
+            return write_frame
+        else:
+            # TODO: out of band
+            pass
 
-    def write_frame(self):
-        # write the whole file
-        while True:
-            buffer = os.read(self.file_fd, 100)
-            l = len(buffer)
-            if l == 0:
-                break
-            os.write(self.archive_fd, buffer)
+    def _define_end_frame(self):
+        if (self.framer_type == "out"):
+            def end_frame():
+                pass
+            return end_frame
+        else:
+            # TODO: out of band
+            pass
 
-    # I don't need this for now
-    # def end_frame(self):
-    #     pass
+    def _define_close(self):
+        if (self.framer_type == "out"):
+            def close():
+                os.close(self.file_fd)
+            return close
+        else:
+            # TODO: out of band
+            pass
 
-    def close(self):
-        os.close(self.file_fd)
-        pass
+
+class Extractor:
+    def __init__(self, framer_type: str, archive_fd: int):
+        self.archive_fd = archive_fd
+        
+        # define functions based on framer type
+        self.framer_type = framer_type
+        self.extract = self._define_extrat()
+        
+    def _define_extrat(self):
+        if (self.framer_type == "out"):
+            def extract():
+                while True:
+                    filename, file_size = read_header(self.archive_fd)
+
+                    # if no headers left, break
+                    if filename == None:
+                        break
+
+                    # make directories if they don't exist
+                    os.makedirs("/".join(filename.split("/")[:-1]), exist_ok=True)
+
+                    # create files with name
+                    file_fd = os.open(filename, os.O_RDWR | os.O_CREAT | os.O_TRUNC)
+
+                    # extract file contents
+                    read_idx = 0
+                    while read_idx < file_size:
+                        buffer_size = min(100, file_size - read_idx)
+                        read_idx += buffer_size
+                        buffer = os.read(self.archive_fd, buffer_size)
+
+                        os.write(file_fd, buffer)
+            return extract
+        else:
+            # TODO:
+            pass
+        
+    # # while there files left in archive
+    # while True:
+    #     filename, file_size = read_header(archive_fd)
+
+    #     # if no headers left, break
+    #     if filename == None:
+    #         break
+
+    #     # make directories if they don't exist
+    #     os.makedirs("/".join(filename.split("/")[:-1]), exist_ok=True)
+
+    #     # create files with name
+    #     file_fd = os.open(filename, os.O_RDWR | os.O_CREAT | os.O_TRUNC)
+
+    #     # extract file contents
+    #     read_idx = 0
+    #     while read_idx < file_size:
+    #         buffer_size = min(100, file_size - read_idx)
+    #         read_idx += buffer_size
+    #         buffer = os.read(archive_fd, buffer_size)
+
+    #         os.write(file_fd, buffer)    
 
 
 def binary_format_64(n: int):
@@ -69,55 +162,38 @@ def read_header(fd: int) -> tuple[str, int]:
     return filename, file_size
 
 
-def archive(filenames, archive_fd=1):
+def archive(filenames, framer_type: str, archive_fd=1):
     # archive_fd = os.open(archive_name, os.O_RDWR | os.O_CREAT | os.O_TRUNC)
 
     # for each filename
     for filename in filenames:
         # framer(filename)
-        framer = Framer(filename, archive_fd)
+        framer = Framer(framer_type, filename, archive_fd)
+
         # framer start frame
         framer.start_frame()
         # framer write frame
         framer.write_frame()
+        # end frame --> only useful in inbound framing
+        framer.end_frame()
         # close
         framer.close()
 
 
-def extract(archive_fd: int):
-    # while there files left in archive
-    while True:
-        filename, file_size = read_header(archive_fd)
-
-        # if no headers left, break
-        if filename == None:
-            break
-
-        # make directories if they don't exist
-        os.makedirs('/'.join(filename.split('/')[:-1]), exist_ok=True)
-
-        # create files with name
-        file_fd = os.open(filename, os.O_RDWR | os.O_CREAT | os.O_TRUNC)
-
-        # extract file contents
-        read_idx = 0
-        while read_idx < file_size:
-            buffer_size = min(100, file_size - read_idx)
-            read_idx += buffer_size
-            buffer = os.read(archive_fd, buffer_size)
-
-            os.write(file_fd, buffer)
+def extract(archive_fd: int, framer_type: str):
+    extractor = Extractor(framer_type, archive_fd)
+    extractor.extract()
 
 
 if __name__ == "__main__":
-    args = sys.argv
-    flag = args[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("operation", choices=['x', 'c'])
+    parser.add_argument("--framer", default="out")
+    parser.add_argument("rest", nargs="*")
+    args = parser.parse_args()
 
-    # error if first argument isn't x or c
-    if flag != "x" and flag != "c":
-        raise Exception("First argument must be either 'x' or 'c'!")
-
-    if flag == "x":
-        extract(0)  # extract from stdin
+    if args.operation == 'x':
+        extract(0, args.framer)  # extract from stdin
     else:
-        archive(args[2:], 1)  # output goes to stdout
+        archive(args.rest, args.framer, 1)  # output goes to stdout    
+    
